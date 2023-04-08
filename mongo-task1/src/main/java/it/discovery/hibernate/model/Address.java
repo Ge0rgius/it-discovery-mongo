3e51c83d49d3f36ade7e4d87ef07f86663cafabd
Sergiy <sergey.morenets@gmail.com>
2023-04-08 10:59:36 +0300

    Task 1

--- mongo-task1/src/main/java/it/discovery/hibernate/model/Address.java
+++ mongo-task1/src/main/java/it/discovery/hibernate/model/Address.java
@@ -1,18 +1,7 @@
 package it.discovery.hibernate.model;
 
 import jakarta.persistence.Embeddable;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 @Embeddable
-public class Address {
-    private String country;
-
-    private String city;
-
-    private String street;
-
-    private String apt;
+public record Address(String country, String city, String street, String apt) {
 }
